918252445fabbe30f44baf15104f158f40343297
taythinh1-spec <taythinh1@gmail.com>
2026-06-27 20:58:27 +0700

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
+#include <memory>
 class IPayment { public: virtual void processPayment(double amount) = 0; virtual ~IPayment() {} };
-class MomoPayment : public IPayment {
-public: void processPayment(double amount) override { std::cout << "Thanh toan Momo\n"; }
-};
-class OrderItem { void* product; int qty; };
-int main() { std::cout << "Them IPayment va OrderItem (Raw pointer)\n"; return 0; }
+class OrderItem { std::shared_ptr<void> product; int qty; };
+int main() { std::unique_ptr<IPayment> payment; std::cout << "Fix leak memory!\n"; return 0; }
